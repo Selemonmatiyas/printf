dac0636aefd3e69e9339ef60f3438f19f5515efa
Selemonmatiyas <selemonmatiyas27@gmail.com>
2022-05-17 00:31:13 +0530

    My commit

---
+++ character_print.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_char - Prints a character from a void pointer
+ * @cp: Void pointer which points to an address where a character is stored
+ * @params: a structure containing info about the argument
+ * Return: Number of characters printed (Always 1)
+ */
+int print_char(void *cp, param params)
+{
+	int c = *(int *)cp;
+	(void)(params);
+
+	_putchar(c);
+	return (1);
+}
