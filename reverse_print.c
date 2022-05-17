ca1c44032596ca195d20c4fcf4141a39e57d8144
Selemonmatiyas <selemonmatiyas27@gmail.com>
2022-05-17 12:06:44 +0530

    My commit

---
+++ reverse_print.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * print_reverse - prints a string in reverse.
+ * @s: a void pointer to a string
+ * @params: a structure containing info about the argument
+ * Return: the number of characters
+ */
+int print_reverse(void *s, param params)
+{
+	char *arr = *(char **)s;
+	int i = 0;
+	int count = 0;
+	(void)(params);
+
+	if (arr == NULL)
+	{
+		_putchar('(');
+		_putchar('n');
+		_putchar('u');
+		_putchar('l');
+		_putchar('l');
+		_putchar(')');
+		return (6);
+	}
+	for (i = 0; arr[i] != '\0'; i++)
+		;
+	i--;
+	for (; i > -1; i--)
+	{
+		_putchar(arr[i]);
+		count++;
+	}
+	return (count);
+}
